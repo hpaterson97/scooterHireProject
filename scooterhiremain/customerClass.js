a871d4debf41b5c322b77d97c55ddc9d8742600c
hpaterson97 <heather.paterson@sky.uk>
2021-10-18 19:18:09 +0100

    added customer class functionality

--- scooterhiremain/customerClass.js
+++ scooterhiremain/customerClass.js
@@ -1,7 +1,12 @@
 class Customer{
-    constructor(name, age) {
+    static customers =[];
+    constructor(name, age, town) {
         this.name = name;
         this.age = age;
+        this.town = town;
+        this.member = false;
+        this.dist_travelled = 0;
+        
 
         if(!this.name) {
             throw new Error('Customer must have name');
@@ -13,8 +18,55 @@ class Customer{
         if(!this.age) {
             throw new Error('Customer must have age');
         }
+        if(typeof this.age !== 'number') {
+            throw new Error('Customer age must be a number');
+        }
+        if(!this.town) {
+            throw new Error('Town must be entered');
+        }
+
+        if(typeof this.town !== 'string') {
+            throw new Error('Town must be a string');
+        }
+
+        if(typeof this.dist_travelled !== 'number') {
+            throw new Error('dist_travelled must be a number');
+        }
+        
+    }
+    setMemberStatus(status) {
+        if (typeof status === 'boolean') {
+            return this.member = status;
+        }
+        else{
+            throw new Error('Member must be boolean');
+        }
+        
+    }
+    checkOver18() {
+        if (this.age >= 18) {
+            return true;
+
+        }
     }
 
+    register() {
+        if (this.checkOver18() == true) {
+            Customer.customers.push(this);
+            this.setMemberStatus(true);
+            console.log('Welcome to our scooter hire app');
+        }
+        else {
+            console.log('Unable to register');
+        }
+    }
 }
+
+const john = new Customer('John', 18, 'town1');
+john.register();
+
+
+
+
 
 module.exports = {Customer};
--- tests/customer.test.js
+++ tests/customer.test.js
@@ -11,6 +11,17 @@ describe('Customer Attribute Tests', ()=>{
         expect(()=>new Customer('John')).toThrowError('Customer must have age');
     });
     test('should throw error if customer age is not a number', ()=>{
-        expect(()=>new Customer(123)).toThrowError('Customer age must be a number');
+        expect(()=>new Customer('John', '123')).toThrowError('Customer age must be a number');
     });
+    test('should throw error if town is not entered', ()=>{
+        expect(()=>new Customer('John', 1)).toThrowError('Town must be entered');
+    });
+    test('should throw error if town is not a string', ()=>{
+        expect(()=>new Customer('John', 1, 3)).toThrowError('Town must be a string');
+    });
+    test('should throw error if member status is set to anything but boolean', ()=>{
+        expect(()=>new Customer('John', 1, 'town1').setMemberStatus(4)).toThrowError('Member must be boolean');
+    });
+    
+
 });
